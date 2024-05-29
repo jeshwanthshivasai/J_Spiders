0010044dd5c323f98260cc6a42a7b19d05f9402c
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-05-29 10:50:42 +0530

    change

--- 290524/DoubleArray.java
+++ 290524/DoubleArray.java
@@ -1,13 +1,28 @@
-public class DoubleArray {
-    public static void main(String[] args) {
-        double [] i = new double[5];
-        i[0] = 10.0;
-        i[1] = 20.0;
-        i[2] = 30.0;
-        i[3] = 40.0;
-        i[4] = 50.0;
-        for (double j=0; j<i.length; j++) {
-            System.out.println(i[j]);
-        }
-    }
-}
+// public class DoubleArray {
+//     public static void main(String[] args) {
+//         double[] i = new double[5];
+//         i[0] = 10.0;
+//         i[1] = 20.0;
+//         i[2] = 30.0;
+//         i[3] = 40.0;
+//         i[4] = 50.0;
+//         for (int j=0; j<i.length; j++) {
+//             System.out.println(i[j]);
+//         }
+//     }
+// }
+
+// public class DoubleArray {
+//     public static void main(String[] args) {
+//         double [] i = {10.0,20.0,30.0,40.0,50.0};
+//         i[0] = 10.0;
+//         i[1] = 20.0;
+//         i[2] = 30.0;
+//         i[3] = 40.0;
+//         i[4] = 50.0;
+//         i[5] = 50.0;
+//         for (int j=0; j<i.length; j++) {
+//             System.out.println(i[j]);
+//         }
+//     }
+// }
