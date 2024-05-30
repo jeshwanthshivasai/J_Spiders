ad55f2099e4821f0557c1065f1d1ff7900d22429
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-05-30 16:39:32 +0530

    oij

--- 300524/StringObject.java
+++ 300524/StringObject.java
@@ -33,30 +33,46 @@
 // }
 
 //String is palindrome or not (CASE-1)
-public class StringObject {
-    public static void main(String[] args) {
-        String s1 = "jeshwanthshivasai";
-        System.out.println(s1.length() + " characters are there in your name.");
-        //print in ascending order
-        for (int i = 0; i < s1.length(); i++) {
-            System.out.println(s1.charAt(i));
-        }
-        System.out.println("Your name in ascending order " + s1);
-        //print in descending order
-        String s2 = "";
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            s2 = s2 + s1.charAt(i);
-        }
-        System.out.println("Your name in descending order " + s2);
-        if (s1.equals(s2)) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO"); 
-        }
-    }
-}
+// public class StringObject {
+//     public static void main(String[] args) {
+//         String s1 = "jeshwanthshivasai";
+//         System.out.println(s1.length() + " characters are there in your name.");
+//         //print in ascending order
+//         for (int i = 0; i < s1.length(); i++) {
+//             System.out.println(s1.charAt(i));
+//         }
+//         System.out.println("Your name in ascending order " + s1);
+//         //print in descending order
+//         String s2 = "";
+//         for (int i = s1.length() - 1; i >= 0; i--) {
+//             s2 = s2 + s1.charAt(i);
+//         }
+//         System.out.println("Your name in descending order " + s2);
+//         if (s1.equals(s2)) {
+//             System.out.println("YES");
+//         } else {
+//             System.out.println("NO"); 
+//         }
+//     }
+// }
 
 //String is palindrome or not (CASE-2)
 public class StringObject {
-    
+    public static void main(String[] args) {
+        String s = "sees";
+        System.out.println(s.length() + " characters are there in the string");
+        //print in ascending order
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+        String s1 = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            s1 = s1 + s.charAt(i);
+        }
+        if (s.equals(s1)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
 }
