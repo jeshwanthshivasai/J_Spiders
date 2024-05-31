032306937f5cc9d656eb31ab50ee592b880e8d4a
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-05-31 13:51:50 +0530

    hj

---
+++ 310524/UncheckedAndCheckedException.java
@@ -0,0 +1,16 @@
+public class UncheckedAndCheckedException {
+    public static void main(String[] args) {
+        run();
+    }
+    public static void run() {
+        int i=10;
+        int j=0;
+        try{
+        int res=i/j;
+        System.out.println(res);
+        }
+        catch(Exception e){
+        System.out.println("Exception while running");
+        }
+    }
+}
