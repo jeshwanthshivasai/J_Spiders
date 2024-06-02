e36693d773b71f2c21053a0458e48ea6fa2ded07
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-06-02 21:56:20 +0530

    hdj

--- 310524/UncheckedAndCheckedException.java
+++ 310524/UncheckedAndCheckedException.java
@@ -1,23 +1,18 @@
-// public class UncheckedAndCheckedException {
-//     public static void main(String[] args) {
-//         run();
-//     }
-//     public static void run() {
-//         int i=10;
-//         int j=0;
-//         try {
-//             int res=i/j;
-//             System.out.println(res);
-//         }
-//         catch(Exception e) {
-//             System.out.println("Exception while running");
-            
-//         }
-//     }
-// }
-
 public class UncheckedAndCheckedException {
     public static void main(String[] args) {
-        
+        run();
+    }
+    public static void run() {
+        int i=10;
+        int j=0;
+        try 
+        {
+            int res=i/j;
+            System.out.println(res);
+        }
+        catch(Exception e) 
+        {
+            System.out.println("Exception while running");
+        }
     }
 }
