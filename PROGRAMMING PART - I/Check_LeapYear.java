c4ec73f43025140d0b66583c6d1f5532c43d485f
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-07-24 11:51:15 +0530

    new

--- PROGRAMMING PART - I/Check_LeapYear.java
+++ PROGRAMMING PART - I/Check_LeapYear.java
@@ -6,7 +6,7 @@ public class Check_LeapYear {
         System.out.print("Enter a year: ");
         int year = sc.nextInt();
         if ((year % 4 == 0 && year % 100!= 0) || year % 400 == 0) {
-            System.out.println(year + " is a leap year");
+            System.out.println(year + " is a leap year");;;;;;
         } else {
             System.out.println(year + " is not a leap year");
         }
