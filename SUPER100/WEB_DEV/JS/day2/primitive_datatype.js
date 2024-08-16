1582decf928bf0e422180b06e4661afdf41fb1fe
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-08-16 10:43:26 +0530

    new

---
+++ SUPER100/WEB_DEV/JS/day2/primitive_datatype.js
@@ -0,0 +1,45 @@
+var x = `hi i'm an architect`;
+console.log(x);
+
+var x;
+console.log(x);
+var y = null;
+console.log(y);
+console.log(typeof y);
+
+console.log(3<2<5>7<0>6<8>9>2<7<6>6<9>87>0)
+
+console.log((2>2) && (3<5))
+
+console.log(!true)
+
+console.log(true + false)
+
+console.log(true + "1")
+
+console.log(true - false)
+
+console.log(true * false)
+
+console.log(null * 0)
+
+console.log(0 + null)
+
+console.log(1 + "1")
+
+console.log(null + null)
+
+console.log(!"false")
+
+console.log(Nan && 0)
+
+console.log(NaN - 8);
+
+var sum1 = Number(prompt("enter a"));
+var sum2 = Number(prompt("enter b"));
+console.log(sum1 + sum2);
+
+var msg = confirm("do you want to access your microphone?");
+console.log(msg);
+
+alert("download successful");
