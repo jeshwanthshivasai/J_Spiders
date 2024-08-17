ea4b3d0945ab45263191853a3d084d3db60bcee1
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-08-17 09:27:24 +0530

    sat test

---
+++ SUPER100/WEB_DEV/JS/day3/m&im_string.js
@@ -0,0 +1,77 @@
+// var x="jesse";
+// var y="james";
+
+// console.log(x+y);
+
+// console.log(x.length);
+// console.log(x.charAt);
+// console.log(x.charCodeAt);
+
+// console.log(x.toUpperCase());
+
+// console.log(x.toLowerCase());
+
+// var x = "mont";
+// var res=x.toUpperCase();
+// console.log(x);
+// console.log(res);
+
+// console.log('jesse' = 'james' == 24);
+
+// console.log(10==10);
+
+
+// var x = Number(prompt("Enter number: "));
+// if (x%2===0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+
+// x = (x%2===0) ? "even" : "odd";
+// console.log(x);
+
+// var num = Number(prompt("Enter number: "));
+// switch (num) {
+//     case 1: console.log("monday");
+//         break;
+//     case 2: console.log("tuesday");
+//         break;
+//     case 3: console.log("wednesday");
+//         break;
+//     case 4: console.log("thursday");
+//         break;
+//     case 5: console.log("friday");
+//         break;
+//     case 6: console.log("saturday");
+//         break;
+//     case 7: console.log("sunday");
+//         break;
+//     default: console.log("Invalid input");
+// }
+
+// var date = new Date();
+// console.log(date);
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.getDate);
+// console.log(date.getMonth);
+// console.log(date.getFullYear);
+// console.log(date.getHours);
+// console.log(date.getMinutes);
+// console.log(date.getSeconds);
+// console.log(date.getMilliseconds);
+
+
+console.log();
+
+
+
+
+
+
+
+
+
+
