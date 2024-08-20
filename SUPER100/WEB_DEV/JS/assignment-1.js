b1ff37fbc5aed2a2517e6e6dd5f6f55c835f31f8
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-08-20 21:56:25 +0530

    new

---
+++ SUPER100/WEB_DEV/JS/assignment-1.js
@@ -0,0 +1,124 @@
+// Retrieving keys(Object.keys())
+var data1={
+    name1:"nanditha",
+    occupation1:"Trainer",
+    subject1:"WebTechnologies",
+    place1:"Jspiders"
+  }
+  console.log(Object.keys(data1));
+  
+
+  var data2={
+    name2:"dhanush",
+    occupation2:"Trainer",
+    subject2:"Java"
+  }
+  console.log(Object.keys(data2));
+
+  var person={
+    name:"Charan",
+    age:22,
+    purshuing:"trinee",
+    place:"Jspiders"
+  }
+  console.log(Object.keys(person));
+  
+  var laptop={
+    make:"Dell",
+    screenSize:15,
+    RAM:"8GB",
+    price:"Rs40,000"
+  }
+  console.log(Object.keys(laptop));
+  
+  var employee={
+    ename:"RajendraPrasad",
+    eid:"209H1A0535",
+    dob:"26-01-1990",
+    des:"javadeveloper"
+  }
+  console.log(Object.keys(employee));
+  
+// Retrieving values(Object.values())
+
+var data1={
+  name1:"nanditha",
+  occupation1:"Trainer",
+  subject1:"WebTechnologies",
+  place1:"Jspiders"
+}
+console.log(Object.values(data1));
+
+
+var data2={
+  name2:"dhanush",
+  occupation2:"Trainer",
+  subject2:"Java"
+}
+console.log(Object.values(data2));
+
+var person={
+  name:"Charan",
+  age:22,
+  purshuing:"trinee",
+  place:"Jspiders"
+}
+console.log(Object.values(person));
+
+var laptop={
+  make:"Dell",
+  screenSize:15,
+  RAM:"8GB",
+  price:"Rs40,000"
+}
+console.log(Object.values(laptop));
+
+var employee={
+  ename:"RajendraPrasad",
+  eid:"209H1A0535",
+  dob:"26-01-1990",
+  des:"javadeveloper"
+}
+console.log(Object.values(employee));
+
+// retrieving entries(Object.entries())
+
+var data1={
+  name1:"nanditha",
+  occupation1:"Trainer",
+  subject1:"WebTechnologies",
+  place1:"Jspiders"
+}
+console.log(Object.entries(data1));
+
+
+var data2={
+  name2:"dhanush",
+  occupation2:"Trainer",
+  subject2:"Java"
+}
+console.log(Object.entries(data2));
+
+var person={
+  name:"Charan",
+  age:22,
+  purshuing:"trinee",
+  place:"Jspiders"
+}
+console.log(Object.entries(person));
+
+var laptop={
+  make:"Dell",
+  screenSize:15,
+  RAM:"8GB",
+  price:"Rs40,000"
+}
+console.log(Object.entries(laptop));
+
+var employee={
+  ename:"RajendraPrasad",
+  eid:"209H1A0535",
+  dob:"26-01-1990",
+  des:"javadeveloper"
+}
+console.log(Object.entries(employee));
