e3bec70dbc80c72492bd180d864956fc7d987a0e
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-08-31 09:23:00 +0530

    new

--- SUPER100/WEB_DEV/JS/day12/dom.js
+++ SUPER100/WEB_DEV/JS/day12/dom.js
@@ -1,7 +1,7 @@
 //getElementById
-let element = document.getElementsByTagName('h1');
-console.log(element);
-element.style.width = '200%';
+// let element = document.getElementsByTagName('h1');
+// console.log(element);
+// element.style.width = '200%';
 
 
 // display date after 3 seconds
@@ -17,9 +17,17 @@ element.style.width = '200%';
 
 // document.write
 
-let date = new Date();
-let button = document.querySelectorAll("button");
-setTimeout (() => {
-    button[0].innerHTML = date.toLocaleString();
-    button[1].innerHTML = date.toLocaleString();
-}, 1000)
+// let date = new Date();
+// let button = document.querySelectorAll("button");
+// setTimeout (() => {
+//     button[0].innerHTML = date.toLocaleString();
+//     button[1].innerHTML = date.toLocaleString();
+// }, 1000)
+
+//add background-image and add linear gradient to it
+let bg = document.getElementById("background");
+// console.log(bg);
+bg.style.height = '100vh';
+bg.style.background = "url('https://picsum.photos/1920/1080')";
+bg.style.backgroundSize = "100vh";
+bg.style.backgroundImage = `linear-gradient(to bottom left, black, red)`;
